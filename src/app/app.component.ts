51caaf40e8bc8023b1ef6130df2696f87d9bf279
Desktop\112233 <laa.210391@gmail.com>
2021-03-09 18:40:55 +0100

    Added some user notes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project1';
+  YES_LOGGED = "Hello $username";
+  NO_LOGGED = "Please, log in"
+  textLogged = "";
+  defaultUSer = "AnonUser";
 
   showAlert(){
 
     alert("this is an alert");
+    
+  }
+
+  login(){
+
+    this.textLogged = this.YES_LOGGED.replace("%username",this.defaultUSer);
+  }
+
+  closeSession(){
+
+    this.textLogged = this.NO_LOGGED;
   }
 }
 
